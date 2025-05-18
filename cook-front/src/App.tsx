d4eeb2617d81e8363279afabb84e91674b8c90e0
cyprien <cyprien.godet@gmail.com>
2025-05-18 19:05:53 +0100

    API V1 testée et validée

---
+++ cook-front/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, Heading } from '@chakra-ui/react'
+
+function App() {
+    return (
+        <div style={{ padding: '2rem' }}>
+            <Heading color="teal.500">Bienvenue sur Cook 🍽️</Heading>
+            <Button colorScheme="teal" mt="4">Cuisiner</Button>
+        </div>
+    )
+}
+
+export default App
