ce6763dfad6492bde8ddc33b67c0c8b7c7987b39
cyprien <cyprien.godet@gmail.com>
2025-06-04 22:21:56 +0100

    Affichage des listes des ingredients c'est amelioré avec des couleurs et bords arrondis

--- cook-front/src/components/MakerPage/IngredientsList.tsx
+++ cook-front/src/components/MakerPage/IngredientsList.tsx
@@ -1,6 +1,6 @@
 import type {Receipe} from "@/components/RecipesGrid.tsx";
 import useData from "@/hooks/useData.ts";
-import {Box, Heading, Table} from "@chakra-ui/react";
+import {Box, Heading, SimpleGrid, Table} from "@chakra-ui/react";
 import type {AxiosRequestConfig} from "axios";
 import qs from 'qs';
 
@@ -27,7 +27,7 @@ const IngredientsList = ({receipes}:Props) => {
         }
     };
 
-    const {data,error,isLoading} = useData<Ingredient>("recettes/ingredients/",config)
+    const {data} = useData<Ingredient>("recettes/ingredients/",config)
 
     const categoriesMap: Record<string, Ingredient[]> = {};
 
@@ -40,10 +40,12 @@ const IngredientsList = ({receipes}:Props) => {
     });
 
     return (
-        <Box>
+        <SimpleGrid columns={{sm:1,md:2,lg:3}} gap={"100px"} padding={10}>
             {Object.entries(categoriesMap).map(([categorie, items]) => (
-                <Box key={categorie} mb={8}>
-                    <Heading size="md" mb={2}>{categorie}</Heading>
+                <Box key={categorie} mb={8} borderRadius={"8px"} gap={"0px"} margin={0} padding={0} overflow={"hidden"}>
+                    <Box background={"yellow.200"} margin={0}  h={"30px"} >
+                        <Heading size="md" mb={2} pl={2}  display="flex" alignItems="center" margin={0} h={"100%"}>{categorie}</Heading>
+                    </Box>
                     <Table.Root size="sm">
                         <Table.Header>
                             <Table.Row>
@@ -62,7 +64,7 @@ const IngredientsList = ({receipes}:Props) => {
                     </Table.Root>
                 </Box>
             ))}
-        </Box>
+        </SimpleGrid>
     );
 };
 
